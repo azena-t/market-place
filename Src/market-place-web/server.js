efa4b49840a991abc40e75257d723ff812940ae5
kippert <kipper.timmins@outlook.com>
2015-04-12 14:54:24 +0100

    Now fully config driven. Taking a punt on the gitignore file

--- Src/market-place-web/server.js
+++ Src/market-place-web/server.js
@@ -2,33 +2,11 @@ var hapi = require('hapi');
 var server = new hapi.Server();
 var config = require('./configuration.js');
 
-/*
-var serverConfig = {
-	port : 8081
-};
+server.connection(config().serverConfig);
 
-var indexConfig = {
-	method : 'GET',
-	path : '/',
-	handler : {
-		file : 'index.html'
-	}
-};
-var staticConfig = {
-    method: 'GET',
-    path: '/static/{param*}',
-    handler: {
-        directory: {
-            path: 'static'
-        }
-    }
-}*/
-
-server.connection(config.serverConfig);
-
-server.route(config.indexConfig);
-server.route(config.staticConfig);
+server.route(config().indexConfig);
+server.route(config().staticConfig);
 
 server.start(function(){
-	console.log("Server running with config: " + serverConfig);
+	console.log("Server running with config: " + config().serverConfig.port);
 });
